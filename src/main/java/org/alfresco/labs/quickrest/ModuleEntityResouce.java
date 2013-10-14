ada8bb141e0f58aa0b26d8d00d8b209d7db56e8b
Gethin James <gethin.james@alfresco.com>
2013-10-14 14:48:36 +0200

    Added get Module by id

--- src/main/java/org/alfresco/labs/quickrest/ModuleEntityResouce.java
+++ src/main/java/org/alfresco/labs/quickrest/ModuleEntityResouce.java
@@ -1,5 +1,7 @@
 package org.alfresco.labs.quickrest;
 
+import org.alfresco.rest.framework.WebApiDescription;
+import org.alfresco.rest.framework.core.exceptions.EntityNotFoundException;
 import org.alfresco.rest.framework.resource.EntityResource;
 import org.alfresco.rest.framework.resource.actions.interfaces.EntityResourceAction;
 import org.alfresco.rest.framework.resource.parameters.CollectionWithPagingInfo;
@@ -9,7 +11,7 @@ import org.alfresco.service.cmr.module.ModuleService;
 import org.springframework.beans.factory.annotation.Autowired;
 
 /**
- * This represents a url endpoint in the api, /mycompany/private/1/modules, see package-info.java
+ * This represents a url endpoint in the api, /private/mycompany/versions/1/modules, see package-info.java
  * 
  * The @EntityResource defines the url endpoint -> "modules"
  * EntityResourceAction."INTERFACE" specifies what CRUD actions this endpoint implements.
@@ -18,15 +20,25 @@ import org.springframework.beans.factory.annotation.Autowired;
  */
 
 @EntityResource(name="modules", title = "Handles module information.")
-public class ModuleEntityResouce implements EntityResourceAction.Read<ModuleDetails>
+public class ModuleEntityResouce implements EntityResourceAction.Read<ModuleDetails>, EntityResourceAction.ReadById<ModuleDetails>
 {
     @Autowired
     private ModuleService moduleService;
 
     @Override
+    @WebApiDescription(title = "Gets all modules", description = "Gets a list of all modules currently installed.")
     public CollectionWithPagingInfo<ModuleDetails> readAll(Parameters params)
     {
         return CollectionWithPagingInfo.asPaged(params.getPaging(), moduleService.getAllModules());
+    }
+
+    @Override
+    @WebApiDescription(title = "Gets a module id", description = "Gets a module by its unique id.")
+    public ModuleDetails readById(String id, Parameters parameters) throws EntityNotFoundException
+    {     
+        ModuleDetails details = moduleService.getModule(id);
+        if (details != null) return details;
+        throw new EntityNotFoundException(id);  //defaults to throw not found
     }
 
 }
